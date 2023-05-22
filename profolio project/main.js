95cc60942c7073b1b5d49ba62a4887fe57c80b22
Htin Kyaw <htinkyaw@Htins-MacBook-Air.local>
2023-05-23 00:57:19 +0900

    update

---
+++ profolio project/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.sliders').ripples({
+  dropRadius: 15,
+  perturbance: 0.03,
+});
+
+$('.profolio').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  gallery: {
+      enabled: true
+    },
+  type: 'image'
+  // other options
+});
+$('#team_members').owlCarousel({
+  item:3,
+  autoplay: true,
+  smartSpeed: 700,
+  loop: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items: 1
+    },
+    480:{
+      items: 2
+    },
+    768:{
+      items: 3
+    }
+  }
+});
+})
